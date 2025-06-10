04f96e7a33701695ccfac0f7e687bf3768e4c0fb
Ryosuke Imai <ryosuke.imai@mb.lecip.co.jp>
2025-06-10 16:10:21 +0900

    modify

--- data-analytics-guides/docusaurus.config.ts
+++ data-analytics-guides/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/lcp-business-SRE/',
+  url: 'https://github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/data-analytics-knowledge-base/data-analytics-guides/',
