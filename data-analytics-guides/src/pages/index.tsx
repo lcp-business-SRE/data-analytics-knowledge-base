8977a876bada9fc77aadac43bc40245a727a19a6
Ryosuke Imai <ryosuke.imai@mb.lecip.co.jp>
2025-06-11 19:26:22 +0900

    TOP画面を修正

--- data-analytics-guides/src/pages/index.tsx
+++ data-analytics-guides/src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`ようこそ ${siteConfig.title}`}
+      description="ようこそLECIP ビジネス開発センター SREチーム データ分析学習サイトへ <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
