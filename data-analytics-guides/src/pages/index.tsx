636b8d52c50fd41af0182e6b139475aeebdace98
Ryosuke Imai <ryosuke.imai@mb.lecip.co.jp>
2025-06-11 12:59:32 +0900

    modify

--- data-analytics-guides/src/pages/index.tsx
+++ data-analytics-guides/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            このサイトの目的 - 5min ⏱️
+            学習コンテンツはこちら
           </Link>
         </div>
       </div>
