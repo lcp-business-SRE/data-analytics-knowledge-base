a2c3d880131513ffcf4f24167ca3f2c384797510
Ryosuke Imai <ryosuke.imai@mb.lecip.co.jp>
2025-07-29 16:48:02 +0900

    factfulness 修正

--- data-analytics-guides/src/theme/MDXComponents.tsx
+++ data-analytics-guides/src/theme/MDXComponents.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import SreGenericChart from '@site/src/components/SreGenericChart';
 
 export default {
   ...MDXComponents,
   YouTube: LiteYouTubeEmbed,
+  // 2. グローバルコンポーネントとして登録する
+  SreGenericChart: SreGenericChart,
 };
